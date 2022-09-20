f9d662ca2fc73ad7fb73f7c947b50d63e5ab13e1
Vera Tikhonova <veratihonova95@gmail.com>
2022-09-21 02:40:10 +0500

    try to change video script

--- source/js/modules/video.js
+++ source/js/modules/video.js
@@ -3,13 +3,19 @@ const allVideo = document.querySelectorAll('[data-video]');
 const playVideo = () => {
   allVideo.forEach((el) => {
     const buttonPlay = el.querySelector('[data-play]');
+    const urlVideo = el.getAttribute('data-url');
     el.classList.remove('is-open');
     el.classList.add('is-close');
 
     buttonPlay.addEventListener('click', function () {
       el.classList.remove('is-close');
       el.classList.add('is-open');
-      el.querySelector('iframe').src = `${el.querySelector('iframe').src}&autoplay=1&mute=0`;
+      let iframe = document.createElement('iframe');
+      let iframe_url = 'https://www.youtube.com/embed/'+urlVideo+'?autoplay=1&mute=0';
+      if(el.getAttribute('data-params'))iframe_url+='&'+el.getAttribute('data-params');
+      iframe.setAttribute('src',iframe_url);
+      iframe.setAttribute('frameborder','0');
+      el.appendChild(iframe);
     });
   });
 };
