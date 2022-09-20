cffa257fa8ee70e2f091bf24e8189a3280598bf0
13epanda <veratihonova95@gmail.com>
2022-09-20 17:01:54 +0500

    added desktop style for coaches

--- source/js/modules/swiper.js
+++ source/js/modules/swiper.js
@@ -1,16 +1,25 @@
- const createSwipers = () => {
-   let swiperCoaches = new Swiper('.coaches__slider', {
-     slidesPerView: 4,
-     spaceBetween: 30,
-     slidesPerGroup: 4,
-     loop: true,
-     loopFillGroupWithBlank: true,
-     navigation: {
-       nextEl: ".swiper-button-next",
-       prevEl: ".swiper-button-prev",
-     },
-   });
-   return swiperCoaches;
- }
+const createSwipers = () => {
+  let swiperCoaches = new Swiper('.coaches__slider', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    breakpoints: {
+      '768': {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      '1200': {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  return swiperCoaches;
+};
 
- export {createSwipers};
+export {createSwipers};
